144d3a033a9999eacb554c28aee898981e09cab2
Sahil <sahil@Sahils-MacBook-Air.local>
2024-11-12 13:02:44 +0530

    final commit

--- file_count_in_a_directory.sh
+++ file_count_in_a_directory.sh
@@ -4,7 +4,7 @@
 
 directory_to_check=$1
 
-while [ "$(ls -A $directory_to_check)" ]; # till the time directory exists keep running the loop
+while [ "$(ls -A $directory_to_check)" ] # till the time directory exists keep running the loop
 do
   file_count=$(ls -1 $directory_to_check | wc -l)
   echo "File count in the directory is $file_count"
